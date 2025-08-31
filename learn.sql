e6acc05e163adac6644acf0b527d2006d17d1b0a
Khalid Abdi <xamza2009@gmail.com>
2025-08-31 23:38:51 +0300

    try create

--- learn.sql
+++ learn.sql
@@ -1,1 +1,11 @@
-SELECT * FROM employees;
+create table test (
+    name VARCHAR(250),
+    age INT,
+    birth_year date
+);
+
+insert into test(name) values
+    ('khalid'),('abdi');
+
+
+select * from test;
