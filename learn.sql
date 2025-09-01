ed631babe37bd20b7b886a99eae02165c5f7e33b
Khalid Abdi <xamza2009@gmail.com>
2025-09-01 23:36:58 +0300

    learn unique in sql

--- learn.sql
+++ learn.sql
@@ -1,11 +1,16 @@
-create table test (
-    name VARCHAR(250),
-    age INT,
-    birth_year date
-);
-
-insert into test(name) values
-    ('khalid'),('abdi');
+-- create table products(
+-- product_id int ,
+-- product_name varchar(100) unique,
+-- price decimal(4,2)
+-- );
 
 
-select * from test;
+insert into products values
+ (1,"humbger",3.99),(2,'fries',1.99),(3,'soda',1.0),(4,'ice creem ',10);
+
+update  products  set price =99 where product_id =3 ;
+
+select *  from products;
+
+
+-- select * from employees
