5b4b2779193e6d69e85b220bf7adb2faf286d455
Khalid Abdi <xamza2009@gmail.com>
2025-09-01 23:44:12 +0300

    learn not null in sql

--- learn.sql
+++ learn.sql
@@ -1,16 +1,6 @@
--- create table products(
--- product_id int ,
--- product_name varchar(100) unique,
--- price decimal(4,2)
--- );
+alter table products modify price decimal (4,2) not null;
 
 
-insert into products values
- (1,"humbger",3.99),(2,'fries',1.99),(3,'soda',1.0),(4,'ice creem ',10);
+select * from products ;
 
-update  products  set price =99 where product_id =3 ;
-
-select *  from products;
-
-
--- select * from employees
+insert into products values(10,'cookie',null);
