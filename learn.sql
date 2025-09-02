af28c092af22f969f2140d7d4e1204b5c9320a99
Khalid Abdi <xamza2009@gmail.com>
2025-09-02 22:33:50 +0300

    learn primary key in mysql

--- learn.sql
+++ learn.sql
@@ -1,16 +1,12 @@
-select * from products ;
-
-alter table products alter price set  default 0;
-
-insert into products(product_id,product_name) values
-(5,"starw"),(6,"napkin"),(7,"fork"),(8,"spoon");
-
-create table transacation(
-transacation_id int,
-amount decimal (5,2),
-transacation_date datetime default now()
+create table transacations (
+transacations_id  int primary key,
+amount decimal(5,2)
 );
 
-select * from transacation;
+select * from transacations
 
-insert into transacation(transacation_id,amount) values(3,8.99);
+
+-- alter table add constraint primary key();
+
+
+insert into transacations values(1003,3.99);
