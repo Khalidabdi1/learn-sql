da1a586e7cd5cf0dfc8d24191c6cbd21ca198b63
Khalid Abdi <xamza2009@gmail.com>
2025-09-02 23:29:05 +0300

    learn foreign key

--- learn.sql
+++ learn.sql
@@ -29,3 +29,50 @@ alter table transacations auto_increment=1000;
 
 set sql_safe_updates=0;
 insert into transacations(amount) values(100.00);
+
+
+-- foreign key 
+
+create table customers(
+customer_id int primary key auto_increment,
+first_name varchar(50),
+last_name varchar(50)
+
+);
+
+insert into customers(first_name,last_name) values
+("khalid","abdi"),
+("hamza","abdi"),
+("salma","abdi"),
+("zozo","abdi");
+
+
+create table transacations (
+transacatios_id int primary key auto_increment,
+amount decimal(5,2),
+customer_id  int ,
+foreign key (customer_id) references customers(customer_id)
+);
+
+select * from transacations;
+select * from customers;
+
+-- delete foreign key
+alter table transacations drop foreign key transacations_ibfk_1;
+
+-- add foreign key ;
+
+alter table transacations add constraint fk_customer_id foreign key(customer_id) references customers(customer_id);
+
+
+delete from transacations ;
+select * from transacations;
+
+alter table transacations auto_increment=1000;
+
+insert into transacations(amount,customer_id) values
+(4.99,3),
+(2.90,2),
+(3.38,3),
+(4.99,1);
+
